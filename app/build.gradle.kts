ea186fc6967a68a9650f1ce50cd20839df03c267
yushinc <cys74351@gmail.com>
2025-01-23 14:22:39 +0900

    feat: 실제 안드로이드 기기 연결위해 sdk 버전 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.guru2"
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
