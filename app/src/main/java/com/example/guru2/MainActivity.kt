5b09b94a524297682bd2de19e33bce6ceb54ee76
SongyeonK <syk193@naver.com>
2025-01-24 12:28:48 +0900

    feat: Update allergy and fridge features

--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -6,39 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
+import com.example.guru2.databinding.ActivityMainBinding // 뷰 바인딩 클래스 임포트
 
 class MainActivity : AppCompatActivity() {
+
+    // 뷰 바인딩 객체 선언
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        // 뷰 바인딩 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Edge-to-Edge 설정 유지
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         // AllergyActivity로 이동 버튼 이벤트
-        val buttonGoToAllergy: Button = findViewById(R.id.buttonGoToAllergy)
-        buttonGoToAllergy.setOnClickListener {
+        binding.buttonGoToAllergy.setOnClickListener {
             val intent = Intent(this, AllergyActivity::class.java)
             startActivity(intent)
         }
 
-        // RecipeActivity로 이동 버튼 이벤트
-        val buttonGoToRecipe: Button = findViewById(R.id.buttonGoToAllergy)
-        buttonGoToRecipe.setOnClickListener {
-            val intent = Intent(this, RecipeActivity::class.java)
-            startActivity(intent)
-        }
-
-        // 레시피 요청 페이지로 이동 버튼 - sample
-        val buttonGoToAskRecipe: Button = findViewById(R.id.buttonGoToAskRecipe)
-        buttonGoToAskRecipe.setOnClickListener {
-            val intent = Intent(this, AskRecipeActivity::class.java)
+        // FridgeActivity로 이동 버튼 이벤트
+        binding.buttonGoToFridge.setOnClickListener {
+            val intent = Intent(this, FridgeActivity::class.java)
             startActivity(intent)
         }
     }
