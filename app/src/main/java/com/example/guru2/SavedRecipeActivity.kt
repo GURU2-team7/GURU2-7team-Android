2281c4f4d59d0ced0436f4117be11781743475ee
eh_JJ <thsk12@gmail.com>
2025-01-26 21:28:52 +0900

    design - savedrecipe

--- app/src/main/java/com/example/guru2/SavedRecipeActivity.kt
+++ app/src/main/java/com/example/guru2/SavedRecipeActivity.kt
@@ -1,28 +1,20 @@
-package com.example.guru2.recipe
+package com.example.guru2
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.guru2.R
 
-class RecipeActivity : AppCompatActivity() {
+class SavedRecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recipe)
-
-
-
-        // 시스템 바 인셋 처리
+        setContentView(R.layout.activity_saved_recipe)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-
-
 }
