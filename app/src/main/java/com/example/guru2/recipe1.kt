6c935bab9969874259b8e5634d983110085196c3
eh_JJ <thsk12@gmail.com>
2025-01-26 14:35:59 +0900

    design-scroll

--- app/src/main/java/com/example/guru2/recipe1.kt
+++ app/src/main/java/com/example/guru2/recipe1.kt
@@ -1,9 +1,7 @@
 package com.example.guru2
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,19 +10,22 @@ import androidx.core.view.WindowInsetsCompat
 class recipe1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // 엣지 투 엣지 활성화
         setContentView(R.layout.activity_recipe1)
 
+        // 메인 뷰를 안전하게 가져오고 인셋 처리
+        val mainView = findViewById<View>(R.id.main)
 
-        // 시스템 바 인셋 처리
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        if (mainView != null) {
+            // 시스템 바 인셋 처리
+            ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        } else {
+            // mainView가 null일 경우 로그를 남기거나 처리
+            println("main view is null")
         }
     }
-
-
-
-
 }
